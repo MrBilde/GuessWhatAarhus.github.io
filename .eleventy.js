487072b1bb1fa1d0c72a3ff2c0b20c99df0a17e8
Kasper Dissing Bargsteen <kb@concordium.com>
2023-02-16 09:41:41 +0100

    Add CNAME file for custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -44,6 +44,9 @@ module.exports = function(eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+  // Copy the CNAME file to /_site
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
